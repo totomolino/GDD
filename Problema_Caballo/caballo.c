523dfebd369e785f877e216bf043d28d8a1a8bac
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-08-30 15:16:45 -0300

    Hice dos boludeces

--- Problema_Caballo/caballo.c
+++ Problema_Caballo/caballo.c
@@ -7,6 +7,13 @@ typedef struct{
     int posY;
 }t_caballo;
 
+t_caballo* iniciarCaballo(int seleccion);
+
+void algoritmo(t_caballo* unCaballo, int** tablero);
+
+
+int tableroCompleto(int tablero[8][8]);
+
 
 int main(){
 
@@ -22,16 +29,40 @@ int main(){
     {0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0}
     };
-    
 
+    int tablero2[8][8] = {
+    {1,1,1,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0}
+    };
+    
+    int tablero3[8][8] = {
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1}
+    };
+/*
     printf("Eliga uno de los cuatro vertices (1- 0|0 , 2- 0|7 , 3- 7|7 , 4- 7|0 )");
     scanf("%d", &seleccion);
 
     t_caballo* unCaballo = iniciarCaballo(seleccion);
 
     algoritmo(unCaballo,tablero);
-
-
+*/  
+    
+    printf("Tablero 1 : %d \n", tableroCompleto(tablero));
+    printf("Tablero 2 : %d \n", tableroCompleto(tablero2));
+    printf("Tablero 3 : %d \n", tableroCompleto(tablero3));
     return 0;
 
 
@@ -68,7 +99,26 @@ t_caballo* iniciarCaballo(int seleccion){
 }
 
 void algoritmo(t_caballo* unCaballo, int** tablero){
-    
 
+
+
+
+}
+
+
+int tableroCompleto(int tablero[8][8]){
+
+    for(int i = 0 ; i < 8 ; i++){
+
+        for(int u = 0; u < 8 ; u++){
+            if(tablero[i][u] == 0){ //Si alguno es 0, retorno 0.
+                return 0;
+            }// si no entra en el if, sigue de largo y devuelve true(1)
+        }
+
+
+    }
+
+    return 1;
 
 }
