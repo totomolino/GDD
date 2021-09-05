a569f77c6f199d7905682f46dc4c94ff58b34e67
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-09-05 11:28:38 -0300

    Hice el 2

--- Ejercicios_SQL/ejercicio_2.sql
+++ Ejercicios_SQL/ejercicio_2.sql
@@ -1,1 +1,6 @@
-select prod_codigo,prod_detalle from Producto order by 
+use GD2015C1
+
+select prod_codigo,prod_detalle,fact_fecha from Producto,Item_Factura,Factura
+where prod_codigo = item_producto and item_numero = fact_numero and year(fact_fecha) = 2012
+order by item_cantidad
+
